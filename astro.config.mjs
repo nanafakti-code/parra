8939862e42b791ae1c3d0b2debf8c69e818e0293
nanafakti-code <nanafakti@gmail.com>
2026-02-23 02:16:17 +0100

    fix: add vercel adapter for hosting support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import preact from '@astrojs/preact';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact({ compat: true })],
   output: 'server',
-  adapter: node({ mode: 'standalone' }),
+  adapter: vercel(),
 });
