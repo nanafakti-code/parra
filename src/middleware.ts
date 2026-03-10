495d3fc697bf907e9c0f025b0a5cefc998e38334
nanafakti-code <nanafakti@gmail.com>
2026-03-11 00:58:05 +0100

    feat(security): actualizar políticas de Content Security para incluir fuentes y scripts de unpkg y jsdelivr

--- src/middleware.ts
+++ src/middleware.ts
@@ -105,9 +105,9 @@ export const onRequest = defineMiddleware(async ({ cookies, locals, request, red
         'Content-Security-Policy',
         [
             "default-src 'self'",
-            "script-src 'self' 'unsafe-inline' https://js.stripe.com https://challenges.cloudflare.com",
+            "script-src 'self' 'unsafe-inline' https://js.stripe.com https://challenges.cloudflare.com https://unpkg.com https://cdn.jsdelivr.net",
             "frame-src https://js.stripe.com https://challenges.cloudflare.com",
-            "connect-src 'self' https://api.stripe.com https://jboxsbtfhkanvnhxuxdd.supabase.co",
+            "connect-src 'self' https://api.stripe.com https://jboxsbtfhkanvnhxuxdd.supabase.co https://unpkg.com https://cdn.jsdelivr.net",
             "img-src 'self' data: https:",
             "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
             "font-src 'self' https://fonts.gstatic.com",
